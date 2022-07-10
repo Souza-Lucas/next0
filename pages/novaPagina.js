6a54896f9bb07c8bc3e90f5eaa56640a94a984a3
Lucas Souza <77822244+Souza-Lucas@users.noreply.github.com>
2022-07-10 18:14:11 -0300

    receita 2

---
+++ pages/novaPagina.js
@@ -0,0 +1,14 @@
+export default function Principal(){
+    return(
+        <div>
+            <h1>Página Receita 02</h1>
+            <MariaPrea/>
+        </div>
+    )
+}
+
+export function MariaPrea(){
+    return(
+        <h2>Morreu Maria Preá</h2>
+    )
+}
